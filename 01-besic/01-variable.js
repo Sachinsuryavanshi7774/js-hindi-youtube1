24521d8579bf3ca689ccbad875e9553ba92bdf9f
sachin sanjay suryavanshi <120563426+Sachinsuryavanshi7774@users.noreply.github.com>
2024-04-29 12:49:53 +0000

    containar done

---
+++ 01-besic/01-variable.js
@@ -0,0 +1,9 @@
+const accountId=1234
+let accountEmail="sachindada7774@gmail.com"
+var accountpassword= "12345"
+accountcity= "pune"
+console.log(accountId);
+accountEmail="Nitin123@gmail.com"
+accountpassword="abc"
+accountcity="tuljapur"
+console.table([accountEmail,accountId,accountpassword,accountcity]);
